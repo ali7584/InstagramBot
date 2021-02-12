9da0866bc34b6d64f375fefcf721e0ec8be14dbd
Ali <ali75847584@gmail.com>
2021-02-12 20:35:35 +0330

    Update instagramBot.py

--- instagramBot.py
+++ instagramBot.py
@@ -18,7 +18,7 @@ comments = [' I am a robotttt', 'Nice ', 'loool very nice! ', 'I like it!', 'Sup
 posts=0
 
 #Chromedriver path. Make sure to have the same Chromedriver version as your Google Chrome browser
-browser = webdriver.Chrome(executable_path= r"C:\Users\Lepy\Desktop\python\chromedriver.exe")  # <----- ENTER PATH HERE 
+browser = webdriver.Chrome(executable_path= r'D:\Programing\Project\Git-Personal\Python\driver_selenium\chromedriver.exe')  # <----- ENTER PATH HERE 
 
 browser.get(('https://www.instagram.com/accounts/login/?source=auth_switcher'))
 sleep(2) 
@@ -31,7 +31,8 @@ def likeAndComm(): # Likes and Comments the first 9 posts
 			post = browser.find_element_by_xpath('/html/body/div[1]/section/main/div/div[1]/div/div['+str(y)+']/div['+str(x)+']') 
 			browser.implicitly_wait(1) 
 			post.click()
-			sleep(2)
+			browser.refresh()
+			sleep(5)
 			postLike = browser.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[3]/section[1]/span[1]').click()
 			#postLike.click() 
 			sleep(2)
@@ -40,11 +41,9 @@ def likeAndComm(): # Likes and Comments the first 9 posts
 			sleep(3)
 			comment = browser.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[3]/section[3]/div/form').click() 
 			print("click2")
-			comment = browser.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[3]/section[3]/div/form/textarea').send_keys(random.choice(comments))	
+			comment = browser.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[3]/section[3]/div/form/textarea').send_keys(random.choice(comments)).send_keys(Keys.ENTER)
 			print("send1")
 			sleep(3)
-			sendComment = browser.find_element_by_xpath('/html/body/div[4]/div[2]/div/article/div[3]/section[3]/div/form/button') 
-			sendComment.click()
 			print("click3")
 			sleep(4)
 			posts+=1
@@ -62,10 +61,10 @@ def likeAndComm(): # Likes and Comments the first 9 posts
 def start():
 	#browser.implicitly_wait(3)  #this is another wait function.If you would like to run the script faster, change all sleep() to this
 	username = browser.find_element_by_name('username')
-	username.send_keys('YourInstagramUsername') # <- INSERT YOUR INSTAGRAM USERNAME HERE -------------------------------------------------------------------------------------------------------------------------
+	username.send_keys('Hosein_2021_90') # <- INSERT YOUR INSTAGRAM USERNAME HERE -------------------------------------------------------------------------------------------------------------------------
 	password = browser.find_element_by_name('password')
-	password.send_keys('YourInstagramPassword') # <- INSERT YOUR INSTAGRAM PASSWORD HERE -----------------------------------------------------------------------------------------------------------------------
-	nextButton = browser.find_element_by_xpath('/html/body/div[1]/section/main/div/article/div/div[1]/div/form/div/div[3]/button	')
+	password.send_keys('ali@@2015ali@@2015') # <- INSERT YOUR INSTAGRAM PASSWORD HERE -----------------------------------------------------------------------------------------------------------------------
+	nextButton = browser.find_element_by_xpath('//button[@type="submit"]')
 	nextButton.click()
 	#browser.quit()
 	sleep(4)
